66faaf02c43a1bb54619b7517132d3410f932c39
lynnleung <lianglin1215@gmail.com>
2016-04-12 15:41:16 +0800

    Functional test now check we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 from distutils.dist import warnings
 from time import sleep
 
 class NewVisitorTest(unittest.TestCase):
+    
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -17,30 +19,34 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000');
         #她注意到网页的标题和头部都包含"To-Do"这个词
         self.assertIn('To-Do', self.browser.title)
-        sleep(5)
+        head_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', head_text)
+        
+        #她被邀请直接输入一个to-do事项
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')   
+        #sleep(5)
+        
+        #她在一个文本框中输入了“Buy a new nipple”
+        #安妮的爱好是喝奶
+        inputbox.send_keys('Buy a new nipple')
+        
+        #她按回车键后，页面更新了
+        #待办事项表格中显示了“1：Buy a new nipple”
+        inputbox.send_keys(Keys.ENTER)
+        
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(any(row.text == '1:Buy a new nipple' for row in rows))
+        
+        #页面中又显示了一个文本框，可以输入其他的待办事项
+        #她输入了“Use a new nipple to drink water”
+        #安妮做事很有条理
+            
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main()
-
-#browser = webdriver.Firefox();
-
-
-#assert 'To-Do' in browser.title, "Browser title was " + browser.title;
-
-
-#应用邀请她输入一个待办事项
-
-#她在一个文本框中输入了“Buy a new nipple”
-#安妮的爱好是鲤鱼打挺
-
-#她按回车键后，页面更新了
-#待办事项表格中显示了“1：Buy a new nipple”
-
-#页面中又显示了一个文本框，可以输入其他的待办事项
-#她输入了“Use a new nipple to drink water”
-#安妮做事很有条理
-
 
 #页面再次更新，她的清单中显示了这两个待办事项
 
