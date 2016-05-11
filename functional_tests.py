6d17de3dca9debe57cbe991d2b4f2cce17b41942
lynnleung <lianglin1215@gmail.com>
2016-05-11 14:08:29 +0800

    finish the page 60

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
     
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+    
     def test_can_start_a_list_and_retrieve_it_later(self):
         #安妮听说有一个很酷的在线待办事项应用
         #她去看了这个应用的首页
@@ -36,16 +41,9 @@ class NewVisitorTest(unittest.TestCase):
         #她按回车键后，页面更新了
         #待办事项表格中显示了“1：Buy a new nipple”
         inputbox.send_keys(Keys.ENTER)
-        
+        self.check_for_row_in_list_table('1: Buy a new nipple')
         time.sleep(5)
-        
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        #self.assertTrue(any(row.text == '1:Buy a new nipple' for row in rows),
-        #                "New to-do item did not appear in table -- its text was:\n%s" %(table.text)
-        
-        self.assertIn('1: Buy a new nipple', [row.text for row in rows])
-        
+                
         #页面中又显示了一个文本框，可以输入其他的待办事项
         #她输入了“Use a new nipple to drink water”
         #安妮做事很有条理
@@ -56,14 +54,23 @@ class NewVisitorTest(unittest.TestCase):
         
         #页面再次更新，她的清单中显示了这两个待办事项
         time.sleep(5)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1:Buy a new nipple', [row.text for row in rows])
-        self.assertIn('2:Use a new nipple to drink water', [row.text for row in rows])
         
+        self.check_for_row_in_list_table('1: Buy a new nipple')
+        self.check_for_row_in_list_table('2: Use a new nipple to drink water')
         
+        #安妮想知道这个网站是否会记住她的清单
+
+
+        #她看到网站为她生成了一个唯一的URL
+        #而且页面中有一些文字解说这个功能
+
+
+        #她访问那个URL,发现她的待办事项列表还在
+
+
+        #她很满意，去睡觉了
+
         
-            
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
