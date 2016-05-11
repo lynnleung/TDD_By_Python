1e4d573ca31ce3eea88fc2da8f17e3306c7d78e2
lynnleung <lianglin1215@gmail.com>
2016-05-11 15:28:23 +0800

    Model for list Items and associated migration/page 64

--- lists/tests.py
+++ lists/tests.py
@@ -7,21 +7,34 @@ from django.core.context_processors import csrf
 
 from lists.views import home_page
 from django.template.context_processors import request
+from lists.models import Item
 
 # Create your tests here.
 
+class ItemModelTest(TestCase):
+    
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+        
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+        
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+        
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        
+        
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
-    
-    #def test_home_page_returns_correct_html(self):
-    #    request = HttpRequest();
-    #    response = home_page(request)
-    #    expected_html = render_to_string('home.html')
-        #self.assertEqual(response.content.decode(), expected_html)
-        #self.assertIn(expected_html, response.content.decode())
-
     
     def test_home_page_can_save_a_POST_request(self):
         request = HttpRequest()
